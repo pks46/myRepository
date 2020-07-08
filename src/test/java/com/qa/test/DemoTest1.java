b58bb00d1e9936c9ddd863698e638b618ed8db4d
pks46 <31074714+pks46@users.noreply.github.com>
2020-07-08 21:31:49 +0530

    first commit

---
+++ src/test/java/com/qa/test/DemoTest1.java
@@ -0,0 +1,40 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+
+	@Test
+	public void sum() {
+		System.out.println("Sum Method");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("Substraction Method");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void div() {
+		System.out.println("division Method");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("mulitiplication Method");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+	
+}
